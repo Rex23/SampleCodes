8086032117e109f26866d838dd861fe981630807
Xiang_Windows <xiang.ren7@gmail.com>
2019-01-09 12:04:51 -0500

    added one more example

--- sample_codes_cuda_c/cuda_c_examples/cuda_c_examples/main.cu
+++ sample_codes_cuda_c/cuda_c_examples/cuda_c_examples/main.cu
@@ -5,19 +5,23 @@
 #include "devicequery.cuh"
 #include "kernel.cuh"
 #include "dot_product.cuh"
+#include "const_memory.cuh"
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
 	//Sample code 1: Query Device Properties
-	//devicequery(argc, argv);
+	devicequery(argc, argv);
 	
 	//Sample code 2: Add two arrays
 	//add_two_arrays_example();
 	
 	//Sample code 3: Dot product of two vectors. The shared memory is used for each block.
-	dot_product();
+	//dot_product();
+
+	//Sample code 4: Constant memory usage
+	const_memory();
 
 	system("pause");
 
