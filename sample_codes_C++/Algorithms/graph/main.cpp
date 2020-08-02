497f357469755ffa9e0656c92bc47c0a7ba0a835
Rex23 <44102767+Rex23@users.noreply.github.com>
2020-08-02 11:05:36 -0500

    Create main.cpp

---
+++ sample_codes_C++/Algorithms/graph/main.cpp
@@ -0,0 +1,14 @@
+#include "Tree.h"
+#include "List.h"
+#include "Thread.h"
+#include "Graph.h"
+
+int main()
+{
+	//test_tree();
+	//test_thread();
+	test_graph();
+
+	system("pause");
+	return 0;
+}
