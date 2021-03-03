c2daca7bf076f98b29554d551b912a3c0f7ac524
Rex23 <44102767+Rex23@users.noreply.github.com>
2021-03-02 22:12:59 -0600

    update readme and update mine sweeper code

--- sample_codes_Python/Mine_Sweeper/mine.py
+++ sample_codes_Python/Mine_Sweeper/mine.py
@@ -7,9 +7,25 @@ class mine:
         self.num_row = num_row
         self.num_col = num_col
         self.num_mines = num_mines
-        self.flags = [[0 for _ in range(num_col)] for _ in range(num_row)] #0: empty, 2: populated, -1: mine, -2: stepped mine
+        self.flags = [[0 for _ in range(num_col)] for _ in range(num_row)] # >=0: empty, -1: mine, -2: stepped mine
+        self.populated = [[0 for _ in range(num_col)] for _ in range(num_row)] # 0: no, 1: yes
         self.mine_locs = self.generate_mine_locations()
+        self.generate_flags()
         self.num_total = self.num_row * self.num_col
+    
+    def generate_flags(self):
+        
+        dirs = ((-1, 0), (+1, 0), (0, +1), (0, -1), (-1, -1), (-1, +1), (+1, -1), (+1, +1))
+        
+        for i in range(self.num_row):
+            for j in range(self.num_col):
+                if self.flags[i][j] != -1:
+                    count = 0
+                    for dir in dirs:
+                        i_new, j_new = i + dir[0], j + dir[1]
+                        if i_new >= 0 and i_new < self.num_row and j_new >= 0 and j_new < self.num_col and self.flags[i][j] == -1:
+                            count += 1
+                    self.flags[i][j] = count
     
     def play(self):
 
@@ -35,25 +51,37 @@ class mine:
 
     def display(self, game_over_flag: int, loc: tuple):
 
-        a_map = dict()
-        a_map[0] = 'E' if game_over_flag else '*'
-        a_map[-1] = 'M' if game_over_flag else '*'
-        a_map[-2] = 'M'
-        a_map[2] = 'E'
+        #a_map = dict()
+        #a_map[0] = 'E' if game_over_flag else '*'
+        #a_map[-1] = 'M' if game_over_flag else '*'
+        #a_map[-2] = 'M'
+        #a_map[2] = 'E'
         
         print("The mine after clicking loc: ", loc)
-        for row in range(self.num_row):
-            print(' '.join([a_map[item] for item in self.flags[row]]))
+        for i in range(self.num_row):
+            for j in range(self.num_col):
+
+                if self.flags[i][j] == -1:
+                    if game_over_flag:
+                        print("M", sep = ", ") #if j == self.num_col - 1 else print("M")
+                    else:
+                        print("*", sep = ", ") #if j == self.num_col - 1 else print("*")
+                else:
+                    if self.populated[i][j] == 1:
+                        print(self.flags[i][j], sep = ", ")
+                    else:
+                        print("*", sep = ", ")
+                    
 
     def mark_empty_cells(self, loc: tuple) -> int:
 
-        dirs = ((-1, 0), (+1, 0), (0, +1), (0, -1))
+        dirs = ((-1, 0), (+1, 0), (0, +1), (0, -1), (-1, -1), (-1, +1), (+1, -1), (+1, +1))
 
         a_q = deque()
 
         a_q.append(loc)
 
-        self.flags[loc[0]][loc[1]] = 2
+        self.populated[loc[0]][loc[1]] = 1
 
         count = 1
 
@@ -64,8 +92,8 @@ class mine:
             for dir in dirs:
                 x_new, y_new = x + dir[0], y + dir[1]
 
-                if x_new >= 0 and x_new < self.num_row and y_new >= 0 and y_new < self.num_col and self.flags[x_new][y_new] == 0:
-                    self.flags[x_new][y_new] = 2
+                if x_new >= 0 and x_new < self.num_row and y_new >= 0 and y_new < self.num_col and self.flags[x_new][y_new] != -1:
+                    self.populated[x_new][y_new] = 1
                     a_q.append((x_new, y_new))
                     count += 1
         
--- sample_codes_Python/README.md
+++ sample_codes_Python/README.md
@@ -1,3 +1,4 @@
+├── Mine_Sweeper     # The Windows mine sweeper game developed using Python 3
 ├── SIFs             # Script files for automatic job submission and results collection of a 3D fracture mechanics problem with embedded interal cracks.
 
 └── README.md
